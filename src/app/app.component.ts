0c6781416c72b14c9baa218c5d0de03792b4b7de
miz-ab <mizaby19@gmail.com>
2024-11-27 15:34:36 +0300

    add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+// import { RouterLink, RouterOutlet } from '@angular/router';
+// import { MatButtonModule } from '@angular/material/button';
+// import {MatDialogModule} from '@angular/material/dialog';
+// import  {MatToolbarModule} from '@angular/material/toolbar';
+// import {MatIconModule} from '@angular/material/icon';
+// import {ReactiveFormsModule } from '@angular/forms';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
